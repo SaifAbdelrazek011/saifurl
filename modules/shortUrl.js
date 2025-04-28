ba54875a004798bc1558e8cbbecd333c889b7361
SaifAbdelrazek011 <saifamr1234567@gmail.com>
2025-04-28 05:08:02 +0300

    Added modules/shortUrl.js

---
+++ modules/shortUrl.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { nanoid } from "nanoid";
+
+const shortUrlSchema = new mongoose.Schema(
+  {
+    full: {
+      type: String,
+      required: true,
+    },
+    short: {
+      type: String,
+      required: true,
+      default: () => nanoid(6),
+    },
+    clicks: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const ShortUrl = mongoose.model("ShortUrl", shortUrlSchema);
+export default ShortUrl;
